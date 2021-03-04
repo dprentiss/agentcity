17fc5d19430f851a596ba3c34bb0d2379944b4b8
dprentiss <dprentiss@gmail.com>
2021-03-04 11:02:13 -0500

    init Lane and LaneAgent objects

---
+++ Lane.java
@@ -0,0 +1,315 @@
+/*
+ * Copyright 2018 David Prentiss
+ */
+
+package sim.app.agentcity;
+import sim.util.*;
+import sim.engine.*;
+//import java.util.Arrays;
+
+public class Lane {
+
+    // Required for serialization
+    private static final long serialVersionUID = 1;
+
+    // Stopper
+    Stoppable stopper;
+
+    // Properties
+    public final int idNum;
+    public final int minX;
+    public final int maxX;
+    public final int minY;
+    public final int maxY;
+    public final int height;
+    public final int width;
+    public final int length;
+    public Int2D[] cells;
+
+    // Variables
+    private LaneAgent controller;
+    /*
+    private Int2D[] approachLegs;
+    private int[] approachLegLength;
+    private Int2D[] departureLegs;
+    */
+
+    // Accessors
+    /*
+    public Int2D[] getDepartureLegs() { return departureLegs; }
+    public Int2D[] getApproachLegs() { return approachLegs; }
+    public int getNumApproachLegs(AgentCity ac, Direction direction) {
+        Int2D cell;
+        int num = 0;
+        for (int i = 0; i < approachLegs.length; i++) {
+            cell = approachLegs[i];
+            if (ac.roadGrid.get(cell.x, cell.y) == direction.toInt()) num++;
+        }
+        return num;
+    }
+    */
+    public void setController(LaneAgent controller) {
+        this.controller = controller;
+    }
+    public LaneAgent getController() { return controller; }
+    /*
+    public int getNumCells() {
+        return (maxX - minX + 1) * (maxY - minY + 1);
+    }
+    public Int2D getOrigin() { return new Int2D(minX, minY); }
+    */
+
+    /*
+    public Direction[] getDirectionsTo(Intersection other) {
+        Direction[] directions = new Direction[2];
+        int xOffset = other.minX - this.minX;
+        int yOffset = other.minY - this.minY;
+        if (xOffset < 0) {
+            directions[0] = Direction.WEST;
+        } else if (xOffset > 0) {
+            directions[0] = Direction.EAST;
+        }
+        if (yOffset < 0) {
+            directions[1] = Direction.NORTH;
+        } else if (yOffset > 0) {
+            directions[1] = Direction.SOUTH;
+        }
+        return directions;
+    }
+
+    public boolean legBlocked(Int2D leg) {
+        return controller.legBlocked(leg);
+    }
+
+    public boolean requestReservation(Vehicle vehicle, long time, Int2D[][] path) {
+        return controller.requestReservation(vehicle, time, path);
+    }
+    public boolean cancelReservation(Vehicle vehicle, boolean complete) {
+        return controller.cancelReservation(vehicle, complete);
+    }
+    */
+
+    public Int2D[] getCells() { return cells; }
+
+    /** Constructor */
+    public Lane(int id, int minX, int maxX, int minY, int maxY, AgentCity ac) {
+        idNum = id;
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        height = maxY - minY + 1;
+        width = maxX - minX + 1;
+        length = height + width;
+        setCells(ac);
+    }
+
+    private void setCells(AgentCity ac) {
+        cells = new Int2D[length];
+        final int x = (width == 0 ? 0 : 1);
+        final int y = (height == 0 ? 0 : 1);
+        for (int i = 0; i < cells.length; i++) {
+            cells[i] = new Int2D(minX + i * x, minY + i * y);
+        }
+    }
+
+    /*
+    // Constructor
+    public Intersection(int id, int minX, int maxX, int minY, int maxY, AgentCity ac) {
+        idNum = id;
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+        setLegs(ac);
+    }
+
+    // Constructor
+    public Intersection(int id, int minX, int maxX, int minY, int maxY) {
+        idNum = id;
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minY = minY;
+        this.maxY = maxY;
+    }
+
+    public boolean inIntersection(int x, int y) {
+        return x <= maxX
+            && x >= minX
+            && y <= maxY
+            && y >= minY;
+    }
+    public boolean inIntersection(Int2D cell) {
+        return inIntersection(cell.x, cell.y);
+    }
+
+    private void setLegs(AgentCity ac) {
+        int width = maxX - minX + 1;
+        int height = maxY - minY + 1;
+        Int2D[] tmpDep = new Int2D[2 * width + 2 * height];
+        Int2D[] tmpApp = new Int2D[2 * width + 2 * height];
+        int index = 0;
+        // look around intersection for approach and departure legs
+        if (minY - 1 >= 0) {
+            for (int x = minX; x <= maxX; x++) {
+                if (ac.roadGrid.field[x][minY - 1]
+                        == Direction.SOUTH.toInt()) {
+                    tmpApp[index] = new Int2D(x, minY - 1);
+                } else if (ac.roadGrid.field[x][minY - 1]
+                        == Direction.NORTH.toInt()) {
+                    tmpDep[index] = new Int2D(x, minY - 1);
+                }
+                index++;
+            }
+        }
+        if (maxY + 1 < ac.gridHeight) {
+            for (int x = minX; x <= maxX; x++) {
+                if (ac.roadGrid.field[x][maxY + 1]
+                        == Direction.NORTH.toInt()) {
+                    tmpApp[index] = new Int2D(x, maxY + 1);
+                } else if (ac.roadGrid.field[x][maxY + 1]
+                        == Direction.SOUTH.toInt()) {
+                    tmpDep[index] = new Int2D(x, maxY + 1);
+                }
+                index++;
+            }
+        }
+        if (minX - 1 >= 0) {
+            for (int y = minY; y <= maxY; y++) {
+                if (ac.roadGrid.field[minX - 1][y]
+                        == Direction.EAST.toInt()) {
+                    tmpApp[index] = new Int2D(minX - 1, y);
+                } else if (ac.roadGrid.field[minX - 1][y]
+                        == Direction.WEST.toInt()) {
+                    tmpDep[index] = new Int2D(minX - 1, y);
+                }
+                index++;
+            }
+        }
+        if (maxX + 1 < ac.gridHeight) {
+            for (int y = minY; y <= maxY; y++) {
+                if (ac.roadGrid.field[maxX + 1][y]
+                        == Direction.WEST.toInt()) {
+                    tmpApp[index] = new Int2D(maxX + 1, y);
+                } else if (ac.roadGrid.field[maxX + 1][y]
+                        == Direction.EAST.toInt()) {
+                    tmpDep[index] = new Int2D(maxX + 1, y);
+                }
+                index++;
+            }
+        }
+
+        // Count approach and departure legs
+        int numApp = 0;
+        int numDep = 0;
+        for (int i = 0; i < tmpApp.length; i++) {
+            if (tmpApp[i] != null) numApp++;
+            if (tmpDep[i] != null) numDep++;
+        }
+
+        // Populate arrays with legs
+        approachLegs = new Int2D[numApp];
+        departureLegs = new Int2D[numDep];
+        index = 0;
+        for (int i = 0; i < tmpApp.length; i++) {
+            if (tmpApp[i] != null) {
+                approachLegs[index] = tmpApp[i];
+                index++;
+            }
+        }
+        index = 0;
+        for (int i = 0; i < tmpDep.length; i++) {
+            if (tmpDep[i] != null) {
+                departureLegs[index] = tmpDep[i];
+                index++;
+            }
+        }
+
+        // Count cells on legs
+        approachLegLength = new int[approachLegs.length];
+        for (int i = 0; i < approachLegLength.length; i++) {
+            approachLegLength[i] = getLegLength(ac, approachLegs[i], true);
+        }
+    }
+
+    private int getLegLength(AgentCity ac, Int2D leg, boolean isApproachLeg) {
+        int count = 0;
+        Direction dir = Direction.byInt(ac.roadGrid.get(leg.x, leg.y));
+        Int2D cell = leg;
+        Direction cellDir = dir;
+        Direction countDir = (isApproachLeg ? dir.opposite(): dir);
+        while (cellDir == dir) {
+            cell = ac.getCellAhead(cell, countDir, 1);
+            cellDir = Direction.byInt(ac.roadGrid.get(cell.x, cell.y));
+            count++;
+        }
+        return count;
+    }
+
+    public Int2D[] getDepartureLegsByDirection(AgentCity ac, Direction dir) {
+        int numLegs = 0; // number of relevant departure legs
+        Int2D[] legs; // array to return
+        // Count relevant departure legs
+        for (int i = 0; i < departureLegs.length; i++) {
+            if (ac.roadGrid.get(departureLegs[i].x, departureLegs[i].y)
+                == dir.toInt()) {
+                numLegs++;
+            }
+        }
+        // Fill new array with relevant departure legs
+        legs = new Int2D[numLegs];
+        int nextIndex = 0;
+        for (int i = 0; i < departureLegs.length; i++) {
+            if (ac.roadGrid.get(departureLegs[i].x, departureLegs[i].y)
+                == dir.toInt()) {
+                legs[nextIndex] = departureLegs[i];
+                nextIndex++;
+            }
+        }
+        return legs;
+    }
+
+    // Get all departure legs execpt Direction dir
+    public Int2D[] getDepartureLegs(AgentCity ac, Direction dir) {
+        int numLegs = 0; // number of relevant departure legs
+        Int2D[] legs; // array to return
+        // Count relevant departure legs
+        for (int i = 0; i < departureLegs.length; i++) {
+            if (ac.roadGrid.get(departureLegs[i].x, departureLegs[i].y)
+                    != dir.opposite().toInt()) {
+                numLegs++;
+            }
+        }
+        // Fill new array with relevant departure legs
+        legs = new Int2D[numLegs];
+        int nextIndex = 0;
+        for (int i = 0; i < departureLegs.length; i++) {
+            if (ac.roadGrid.get(departureLegs[i].x, departureLegs[i].y)
+                    != dir.opposite().toInt()) {
+                legs[nextIndex] = departureLegs[i];
+                nextIndex++;
+            }
+        }
+        return legs;
+    }
+
+    public Int2D getDepartureLeg(AgentCity ac, Direction dir,
+                                   Direction laneDir) {
+        Int2D[] departureLegs = getDepartureLegsByDirection(ac, dir);
+        int[] departureInt = new int[departureLegs.length];
+        int nextDepartureIdx = 0;
+        int xOffset = laneDir.getXOffset();
+        int yOffset = laneDir.getYOffset();
+        for (int i = 0; i < departureLegs.length; i++) {
+            departureInt[i] =
+                departureLegs[i].x * xOffset + departureLegs[i].y * yOffset;
+        }
+        for (int j = 1; j < departureInt.length; j++) {
+            if (departureInt[j] > departureInt[nextDepartureIdx]) {
+                nextDepartureIdx = j;
+            }
+        }
+        return departureLegs[nextDepartureIdx];
+    }
+*/
+}
---
+++ LaneAgent.java
@@ -0,0 +1,459 @@
+/*
+ * Copyright 2018 David Prentiss
+ */
+
+package sim.app.agentcity;
+//import sim.util.*;
+import sim.engine.*;
+//import java.util.Arrays;
+
+/** LaneAgent objects control traffic for a specific, contiguous
+ * Lane in the grid.
+ *
+ * @author David Prentiss
+ */
+public class LaneAgent implements Steppable {
+
+    /** Required by MASON for serialization. */
+    private static final long serialVersionUID = 1;
+
+    // Stopper
+    Stoppable stopper;
+
+    // Utility
+    /*
+    private static final int NONE = 0;
+    private static final int SCHEDULE = 1;
+    private static final int intIdNum = 0;
+
+    */
+    // Properties
+    public final int idNum;
+    public Lane lane;
+    public int width;
+    public int height;
+    /*
+    public int scheduleSize;
+    */
+
+    // Variables
+    private AgentCity ac; // state
+    private long step;
+    /*
+    private Int2D[][] cells;
+    private Int2D[] approachLegs;
+    private Vehicle[][][] schedule;
+    private Bag vehicles;
+    private boolean acceptingReservations;
+    private boolean reservationPriority;
+
+    // Reporting variables
+    public int[] reservationsCompleted;
+    public int[] reservationsCanceled;
+    public int scheduleInvalid = 0;
+
+    // Accessors
+    public void setPriority(boolean priority) { reservationPriority = priority; }
+    public boolean getPriority() { return reservationPriority; }
+
+    public Bag getVehicles() { return vehicles; }
+    */
+
+    /** Gets the Lane object controlled by this LaneAgent. */
+    public Lane getLane() { return lane; }
+
+    /** Sets the provided Lane object to be controlled by this
+     * LaneAgent.
+     *
+     * @param lane the Lane object to be controlled by this
+     * LaneAgent.
+     */
+    public void setLane(Lane lane) {
+        this.lane = lane;
+        this.width = lane.width;
+        this.height = lane.height;
+        //TODO remove hard-coded scheduleSize bias
+        /*
+        scheduleSize = width + height + 1;
+        vehicles = new Bag((width + 2) * (height + 2));
+        approachLegs = intersection.getApproachLegs();
+        schedule = new Vehicle[scheduleSize][width][height];
+        cells = new Int2D[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                cells[i][j] = new Int2D(intersection.minX + i,
+                                        intersection.minY + j);
+            }
+        }
+        intersection.setController(this);
+        clearSchedule();
+        */
+    }
+
+    /** Creates and LaneAgent object with the provided ID number and
+     * associated Lane object.
+     *
+     * @param id the ID number of this LaneAgent.
+     * This number should be unique but this is not enforced.
+     */
+    public LaneAgent(int id, Lane lane) {
+        this.idNum = id;
+        /*
+        this.reservationsCanceled = new int[ac.PASSENGER_CAP+1];
+        this.reservationsCompleted = new int[ac.PASSENGER_CAP+1];
+        setPriority(ac.RESERVATION_PRIORITY
+                    && width * height >= ac.MIN_INTERSECTION_CONTROL_SIZE);
+        */
+        setLane(lane);
+    }
+
+
+    /*
+    public boolean requestReservation(Vehicle vehicle, long time,
+                                      Int2D[][] path) {
+        // check path against schedule
+        return addVehicleToSchedule(vehicle, time, path);
+    }
+
+    public boolean cancelReservation(Vehicle vehicle, boolean complete) {
+        boolean canceled = removeVehicleFromSchedule(vehicle);
+        if (canceled) {
+            DriverAgent driver = (DriverAgent)vehicle.getDriver();
+            int numPassengers = vehicle.getNumPassengers();
+            vehicle.hasReservation = false;
+            driver.hasReservation = false;
+            driver.checkReservation(ac);
+            if (complete) {
+                reservationsCompleted[numPassengers]++;
+            } else {
+                reservationsCanceled[numPassengers]++;
+            }
+        }
+        return canceled;
+    }
+
+    public String toString(int option) {
+        int digits = 4;
+        StringBuilder s = new StringBuilder();
+        s.append(String.format("IntersectionAgent[%d]", idNum));
+        s.append(String.format(" step[%d] schedule[%d/%d]",
+                               step, step % scheduleSize, scheduleSize - 1));
+        s.append("\n");
+        switch (option) {
+        case SCHEDULE:
+            for (int i = 0; i < height; i++) {
+                if (step % scheduleSize == 0) {
+                    s.append("\u2551");
+                } else {
+                    s.append("\u2502");
+                }
+                for (int j = 0; j < scheduleSize; j++) {
+                    for (int k = 0; k < width; k++) {
+                        if (schedule[j][k][i] != null) {
+                            s.append(String.format("%"+digits+"d",
+                                                   schedule[j][k][i].idNum));
+                        } else {
+                            s.append(String.format("%"+digits+"d", -1));
+                        }
+                    }
+                    if (j == step % scheduleSize
+                        || j == step % scheduleSize - 1) {
+                        s.append("\u2551");
+                    } else {
+                        s.append("\u2502");
+                    }
+                }
+                s.append("\n");
+            }
+        }
+        return s.toString();
+    }
+
+    public String toString() {
+        return toString(NONE);
+    }
+
+    private int getVehiclePriority(Vehicle vehicle) {
+        int priority = 0;
+        DriverAgent driver = (DriverAgent)vehicle.getDriver();
+        Int2D leg = driver.nextApproachLeg;
+        priority += vehicle.getNumPassengers();
+        priority += countVehicles(ac, leg, true);
+        //System.out.println(vehicle);
+        //System.out.println(priority);
+        return priority;
+    }
+
+    private int countVehicles(AgentCity ac, Int2D leg, boolean isApproachLeg) {
+        int count = 0;
+        Direction dir = Direction.byInt(ac.roadGrid.get(leg.x, leg.y));
+        Int2D cell = leg;
+        Direction cellDir = dir;
+        Direction countDir = (isApproachLeg ? dir.opposite(): dir);
+        while (cellDir == dir) {
+            cell = ac.getCellAhead(cell, countDir, 1);
+            cellDir = Direction.byInt(ac.roadGrid.get(cell.x, cell.y));
+            if (ac.agentGrid.getObjectsAtLocation(cell.x, cell.y) != null) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean addVehicleToSchedule(Vehicle vehicle,
+                                         long time,
+                                         Int2D[][] path) {
+        int x;
+        int y;
+        int timeIndex;
+        boolean hasPriority;
+        Vehicle otherVehicle = null;
+        DriverAgent otherDriver = null;
+        // check if accepting reservations
+        if (!acceptingReservations && !vehicles.contains(vehicle)) return false;
+        // Check if Vehicle's desired path is free
+        for (int i = 0; i < path.length; i++) {
+            timeIndex = (int)((time + i) % scheduleSize);
+            for (int j = 0; j < path[i].length; j++) {
+                if (path[i][j] != null
+                    && intersection.inIntersection(path[i][j])) {
+                    x = path[i][j].x - intersection.minX;
+                    y = path[i][j].y - intersection.minY;
+                    // if reservation conflicts with existing vehicle
+                    if (schedule[timeIndex][x][y] != null) {
+                        if (reservationPriority) {
+                            otherVehicle = schedule[timeIndex][x][y];
+                            otherDriver = (DriverAgent)otherVehicle.getDriver();
+                            otherDriver.updateState(ac);
+                            hasPriority = getVehiclePriority(otherVehicle)
+                                >= getVehiclePriority(vehicle);
+                            if (hasPriority || otherDriver.inIntersection) {
+                                return false;
+                            }
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        // Place vehicle on schedule
+        for (int i = 0; i < path.length; i++) {
+            timeIndex = (int)((time + i) % scheduleSize);
+            for (int j = 0; j < path[i].length; j++) {
+                if (path[i][j] != null
+                    && intersection.inIntersection(path[i][j])) {
+                    x = path[i][j].x - intersection.minX;
+                    y = path[i][j].y - intersection.minY;
+                    if (reservationPriority
+                        && schedule[timeIndex][x][y] != null) {
+                        otherVehicle = schedule[timeIndex][x][y];
+                        cancelReservation(otherVehicle, false);
+                        //if (otherVehicle.getLocation() == ac.getCellAhead(vehicle.getLocation(),vehicle.getDirection(),1)) {
+                            if (false) {
+                            System.out.print(vehicle.toString());
+                            System.out.print(otherVehicle.toString());
+                            System.out.println(getVehiclePriority(vehicle));
+                            System.out.println(getVehiclePriority(otherVehicle));
+                            System.out.print("\n");
+                        }
+                    }
+                    schedule[timeIndex][x][y] = vehicle;
+                }
+            }
+        }
+        if (!vehicles.contains(vehicle)) {
+        	vehicles.add(vehicle);
+        }
+        if (intersection.idNum == intIdNum) {
+            System.out.println();
+            System.out.print(toString(SCHEDULE));
+        }
+        return true;
+    }
+
+    boolean removeVehicleFromSchedule(Vehicle vehicle) {
+        boolean removed = vehicles.remove(vehicle);
+        if (removed) {
+            for (int i = 0; i < scheduleSize; i++) {
+                for (int j = 0; j < width; j++) {
+                    for (int k = 0; k < height; k++) {
+                        if (schedule[i][j][k] == vehicle) {
+                            schedule[i][j][k] = null;
+                        }
+                    }
+                }
+            }
+        }
+        return removed;
+    }
+
+    private boolean scheduleValid(AgentCity ac) {
+        int x;
+        int y;
+        Vehicle vehicle;
+        Bag bag;
+        long steps = ac.schedule.getSteps();
+        int step = (int)(steps % scheduleSize);
+        // Loop over Vehicle with reservations and check if they are on
+        // schedule
+        for (int i = 0; i < vehicles.numObjs; i++) {
+            vehicle = (Vehicle)vehicles.objs[i];
+            DriverAgent driver = (DriverAgent)vehicle.getDriver();
+            long reservationTime = driver.reservationTime;
+            int timeIndex = (int)(steps - reservationTime);
+            boolean cannotLeave =
+                (timeIndex >= 0
+                 && timeIndex < driver.reservationPath.length
+                 && !vehicle.location.equals(driver.reservationPath[timeIndex][0]));
+            if (cannotLeave) {
+                return false;
+            }
+        }
+        // Loop over locations in intersection and check if vehicles match
+        // schedule
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                x = cells[i][j].x;
+                y = cells[i][j].y;
+                vehicle = schedule[step][i][j];
+                bag = ac.agentGrid.getObjectsAtLocation(x, y);
+                if (bag != null) {
+                    if (vehicle != (Vehicle)bag.objs[0]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void allowPriorityReservations(AgentCity ac) {
+        int x;
+        int y;
+        Vehicle vehicle;
+        Bag bag;
+        long steps = ac.schedule.getSteps();
+        vehicles.clear();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                x = cells[i][j].x;
+                y = cells[i][j].y;
+                bag = ac.agentGrid.getObjectsAtLocation(x, y);
+                if (bag == null) continue;
+                vehicle = (Vehicle)bag.objs[0];
+                schedule[(int)(steps % scheduleSize)][i][j] = vehicle;
+                vehicles.add(vehicle);
+            }
+        }
+        vehicles.shuffle(ac.random);
+        for (int k = 0; k < vehicles.numObjs; k++) {
+            vehicle = (Vehicle)vehicles.objs[k];
+            ((DriverAgent)vehicle.getDriver()).updateReservation(ac);
+        }
+        if (intersection.getNumCells() <= 4) {
+            acceptingReservations = true;
+            return;
+        }
+    }
+
+    private void checkSchedule() {
+        int x;
+        int y;
+        Vehicle vehicle;
+        Driver driver;
+        Bag bag;
+        long steps = ac.schedule.getSteps();
+        // If schedule is wrong revoke all reservations and clear schedule
+        if (!scheduleValid(ac)) {
+            if (this.idNum == intIdNum) {
+                System.out.printf("*** Schedule not valid at intersection %d\n",
+                                  this.idNum);
+
+                System.out.print(this.toString(SCHEDULE));
+            }
+            acceptingReservations = false;
+            clearSchedule();
+            // Add Vehicles already in the intersection to schedule and bag
+            allowPriorityReservations(ac);
+            scheduleInvalid++;
+        } else {
+            acceptingReservations = true;
+        }
+    }
+
+    public boolean legBlocked(Int2D leg) {
+        int x = leg.x;
+        int y = leg.y;
+        Bag b;
+        int numCells = ac.NUM_BLOCKED_LEG_CELLS;
+        Direction dir = Direction.byInt(ac.roadGrid.get(x, y));
+            for (int i = 0; i < numCells; i++) {
+                b = ac.agentGrid.getObjectsAtLocation(x, y);
+            if (b != null) {
+                Vehicle vehicle = (Vehicle)b.objs[0];
+                if (vehicle.getSpeed() == 0) {
+                    return true;
+                }
+            }
+            x = x + dir.getXOffset();
+            y = y + dir.getYOffset();
+        }
+        return false;
+    }
+
+    private void clearSchedule() {
+        Vehicle v;
+        // clear every cell of the schedule
+        for (int i = 0; i < scheduleSize; i++) {
+            for (int j = 0; j < width; j++) {
+                for (int k = 0; k < height; k++) {
+                    schedule[i][j][k] = null;
+                }
+            }
+        }
+        // cancel all reservations
+        for (int i = 0; i < vehicles.numObjs; i++) {
+            v = (Vehicle)vehicles.objs[i];
+            v.hasReservation = false;
+            ((DriverAgent)v.getDriver()).hasReservation = false;
+        }
+    }
+
+    private void trimSchedule(AgentCity ac) {
+        Vehicle v;
+        long steps = ac.schedule.getSteps();
+        int step = (int)((scheduleSize + steps - 1) % scheduleSize);
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < height; k++) {
+                schedule[step][j][k] = null;
+            }
+        }
+        // remove vehicles without reservations from Bag
+        for (int i = 0; i < vehicles.numObjs; i++) {
+            v = (Vehicle)vehicles.objs[i];
+            if (!v.hasReservation) {
+                vehicles.removeNondestructively(i);
+            }
+            vehicles.shrink(0);
+        }
+    }
+
+    */
+    public void step(final SimState state) {
+        /*
+        ac = (AgentCity)state;
+        step = ac.schedule.getSteps();
+        trimSchedule(ac);
+        checkSchedule();
+        if (intersection.idNum == intIdNum) {
+            System.out.println();
+            System.out.printf("Step %d, schedule %d/%d\n",
+                              step,
+                              step % scheduleSize,
+                              scheduleSize);
+            System.out.print(toString(SCHEDULE));
+        }
+        */
+    }
+}
